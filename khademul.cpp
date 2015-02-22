074e968ab3e8a05551111e88ed7c2e7ffd9edafb
khademul <khademul144@gmail.com>
2015-02-22 15:34:40 -0600

    version 2
    code modified with additional message

--- khademul.cpp
+++ khademul.cpp
@@ -9,7 +9,14 @@ using namespace std;
 
 int main (){
 
+	cout<<"I was able to compile this code using the HPC at the University of Memphis.\n";
+	cout<<"When I compiled it there, it did not produce any warning message.\n";
+	cout<<"The HPC uses a GNU C++ compiler that can be \n";
+	cout<<"considered a good up-to-date standard.\n";
+	cout<<"I also version-controlled this code using git, \n";
+	cout<<"and used a remote repository hosted by github. \n";
+	cout<<"If I can do this, so can you!!!\n";
 
-
+	getchar();
 	return 0;
 }
